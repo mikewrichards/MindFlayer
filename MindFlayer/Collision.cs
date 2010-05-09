cf26f615bab2eff7643be4783de58c6bb196f731
mikewrichards <mikewrichards@gmail.com>
2010-05-08 20:47:53 -0400

    Added Game.cs, Collision.cs
    Game.cs contains the Game class, used to track all events in the game.
    Collision.cs contains the Collision class, used to track collision instances.

---
+++ MindFlayer/Collision.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MindFlayer
+{
+    class Collision
+    {
+        int objectA, objectB;
+    }
+}
---
+++ MindFlayer/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace MindFlayer
+{
+    class Game
+    {
+        Rectangle border;
+        public void Update()
+        {
+        }
+
+        public Stack<Collision> GetCollisions()
+        {
+            return new Stack<Collision>();
+        }
+
+        public void HandleCollisions(Stack<Collision> collisions)
+        {
+        }
+
+        public void purgeDead()
+        {
+        }
+    }
+}
